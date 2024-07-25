01a8af12b176b7ab2bd4307f5632c8790e6623fc
Nikolay <nickshamrock1997@gmail.com>
2024-07-25 17:57:31 +0300

    test and install Vue Routing

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 
 <template>
   <h2 class="visible bg-orange-500 text-center text-2xl">
-    Привет, мир! Скоро тут будет интернет-магазин
+    Привет, мир! Скоро тут будет новый интернет-магазин
   </h2>
+  <p><a href="/test">Ссылка на тестовый роутер</a></p>
+  <router-view></router-view>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import TestRouter from './components/TestRouter.vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+const router = createRouter({
+  routes: [
+    {
+      path: '/test',
+      component: TestRouter
+    }
+  ],
+  history: createWebHistory()
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
