9b50796c831823248269503e1a0760ad8fa4e2d4
Nikolay <nickshamrock1997@gmail.com>
2024-08-06 19:05:47 +0300

    finished 80% od second section + corrected link fonts

---
+++ src/components/HeaderMenuTransparent.vue
@@ -0,0 +1,42 @@
+<template>
+  <header class="mb-5 h-fit w-full">
+    <nav class="flex flex-wrap items-center justify-start pl-40 pr-40">
+      <img src="/logo.png " class="mr-14 block" />
+      <div class="flex flex-wrap gap-14">
+        <a
+          href="#"
+          class="font-[Oswald] text-sm uppercase leading-5 tracking-widest text-white hover:text-[#43FFD2] hover:underline hover:underline-offset-2"
+          >Каталог</a
+        >
+        <a
+          href="#"
+          class="font-[Oswald] text-sm uppercase leading-5 tracking-widest text-white hover:text-[#43FFD2] hover:underline hover:underline-offset-2"
+          >Доставка и оплата</a
+        >
+        <a
+          href="#"
+          class="font-[Oswald] text-sm uppercase leading-5 tracking-widest text-white hover:text-[#43FFD2] hover:underline hover:underline-offset-2"
+          >О нас</a
+        >
+        <a
+          href="#"
+          class="font-[Oswald] text-sm uppercase leading-5 tracking-widest text-white hover:text-[#43FFD2] hover:underline hover:underline-offset-2"
+          >Контакты</a
+        >
+        <a
+          href="#"
+          class="font-[Oswald] text-sm uppercase leading-5 tracking-widest text-white hover:text-[#43FFD2] hover:underline hover:underline-offset-2"
+          >FAQ</a
+        >
+        <div class="mr-16 flex hover:cursor-pointer">
+          <img src="/icons/search-icon.svg" height="18px" width="18px" class="mr-3 block" />
+          <input
+            class="block w-24 bg-black bg-transparent font-[Oswald] text-sm uppercase leading-5 tracking-widest text-white outline-none placeholder:text-white"
+            placeholder="Поиск"
+            id="search-input"
+          />
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
--- src/components/router-components/MainPage.vue
+++ src/components/router-components/MainPage.vue
@@ -1,55 +1,18 @@
 <script setup>
 import SocialMenuVerticalView from '../SocialMenuVerticalView.vue';
+import HeaderMenuTransparent from '../HeaderMenuTransparent.vue';
 </script>
 
 <template>
-  <section class="h-[1244px] w-full">
+  <section class="h-[1244px] w-full" id="first-section">
     <div
       class="relative h-full w-full bg-[url('/images/expiremental-background.png')] bg-cover bg-center bg-no-repeat"
     >
-      <header class="mb-5 h-fit w-full">
-        <nav class="flex flex-wrap items-center justify-start pl-40 pr-40">
-          <img src="/logo.png " class="mr-14 block" />
-          <div class="flex flex-wrap gap-14">
-            <a
-              href="#"
-              class="font-[Oswald] text-sm uppercase leading-5 tracking-widest text-white hover:text-[#43FFD2] hover:underline hover:underline-offset-2"
-              >Каталог</a
-            >
-            <a
-              href="#"
-              class="font-[Oswald] text-sm uppercase leading-5 tracking-widest text-white hover:text-[#43FFD2] hover:underline hover:underline-offset-2"
-              >Доставка и оплата</a
-            >
-            <a
-              href="#"
-              class="font-[Oswald] text-sm uppercase leading-5 tracking-widest text-white hover:text-[#43FFD2] hover:underline hover:underline-offset-2"
-              >О нас</a
-            >
-            <a
-              href="#"
-              class="font-[Oswald] text-sm uppercase leading-5 tracking-widest text-white hover:text-[#43FFD2] hover:underline hover:underline-offset-2"
-              >Контакты</a
-            >
-            <a
-              href="#"
-              class="font-[Oswald] text-sm uppercase leading-5 tracking-widest text-white hover:text-[#43FFD2] hover:underline hover:underline-offset-2"
-              >FAQ</a
-            >
-            <div class="mr-16 flex hover:cursor-pointer">
-              <img src="/icons/search-icon.svg" height="18px" width="18px" class="mr-3 block" />
-              <input
-                class="block w-24 bg-black font-[Oswald] text-sm uppercase leading-5 tracking-widest text-white outline-none placeholder:text-white"
-                placeholder="Поиск"
-              />
-            </div>
-          </div>
-        </nav>
-      </header>
+      <header-menu-transparent></header-menu-transparent>
 
       <div class="flex flex-col items-center justify-center">
         <div
-          class="text-center font-['Cormorant_Garamond'] text-[200px] font-light uppercase leading-[242px] tracking-[29px] text-white max-sm:hidden"
+          class="text-center font-['Cormorant'] text-[200px] font-light uppercase leading-[242px] tracking-[29px] text-white max-sm:hidden"
         >
           <p class="mb-[-90px]">Lover</p>
           <h1>Flower</h1>
@@ -71,6 +34,113 @@ import SocialMenuVerticalView from '../SocialMenuVerticalView.vue';
       <div class="gradient absolute bottom-0 z-40 -mb-1 h-1/3 w-full"></div>
       <img src="/images/lover-flower-signature.png" class="absolute bottom-2 right-24 z-50" />
     </div>
+  </section>
+
+  <section
+    class="relative flex h-[1029px] w-full justify-between bg-black pl-[165px] pr-[165px]"
+    id="second-section"
+  >
+    <div class="z-50 mt-[100px] flex flex-col">
+      <h2
+        class="mb-[10px] font-['Cormorant'] text-[100px] uppercase leading-[121px] tracking-wide text-white"
+      >
+        каталог
+      </h2>
+      <div class="mb-[81px] ml-[95px] flex w-[550px] flex-col gap-[30px]">
+        <p class="font-['Oswald'] text-xl font-light leading-[26px] tracking-wider text-white">
+          У нас самый большой выбор цветов, букетов, открыток и подарков. Мы всегда поможем вам
+          подобрать букет для вашего события, наш менеджер вас проконсультирует и поможет
+          определиться с выбором
+        </p>
+        <p class="font-['Oswald'] text-xl font-light leading-[29px] tracking-wide text-white">
+          Ознакомьтесь с нашими разделами каталога
+        </p>
+      </div>
+
+      <div
+        class="flex h-[310px] w-[540px] flex-col rounded-[20px] pb-[30px] pl-[31px] pr-[30px] pt-[30px] backdrop-blur-3xl"
+      >
+        <ul
+          class="mb-auto ml-5 list-disc font-[Oswald] text-lg uppercase leading-[27px] tracking-wider text-white"
+        >
+          <h3
+            class="mb-[10px] font-['Oswald'] text-3xl font-bold uppercase leading-[44px] tracking-wider text-[#43FFD2]"
+          >
+            Цветы
+          </h3>
+          <li>Сборные букеты</li>
+          <li>Монобукеты</li>
+          <li>Композиции из цветов</li>
+          <li>розы</li>
+          <li>свадебные</li>
+        </ul>
+        <a
+          href="#"
+          class="self-end font-['Oswald'] text-xs font-bold uppercase leading-[18px] tracking-widest text-[#D978AC] underline underline-offset-1"
+        >
+          смотреть каталог</a
+        >
+      </div>
+    </div>
+
+    <div class="z-50 mt-[231px] flex flex-col items-end gap-[30px]">
+      <div
+        class="flex h-[281px] w-[445px] flex-col rounded-[20px] pb-[30px] pl-[31px] pr-[30px] pt-[30px] backdrop-blur-3xl"
+      >
+        <ul
+          class="mb-auto ml-5 list-disc font-[Oswald] text-lg uppercase leading-[27px] tracking-wider text-white"
+        >
+          <h3
+            class="mb-[10px] font-['Oswald'] text-3xl font-bold uppercase leading-[44px] tracking-wider text-[#43FFD2]"
+          >
+            готовые букеты<br />из сухоцветов
+          </h3>
+          <li>букеты</li>
+          <li>для интерьера</li>
+          <li>Композиции</li>
+        </ul>
+        <a
+          href="#"
+          class="self-end font-['Oswald'] text-xs font-bold uppercase leading-[18px] tracking-widest text-[#D978AC] underline underline-offset-1"
+        >
+          смотреть каталог</a
+        >
+      </div>
+
+      <div
+        class="flex h-[264px] w-[540px] flex-col rounded-[20px] pb-[30px] pl-[50px] pr-[30px] pt-[30px] backdrop-blur-3xl"
+      >
+        <ul
+          class="mb-auto ml-5 list-disc font-[Oswald] text-lg uppercase leading-[27px] tracking-wider text-white"
+        >
+          <h3
+            class="mb-[10px] font-['Oswald'] text-3xl font-bold uppercase leading-[44px] tracking-wider text-[#43FFD2]"
+          >
+            дополнительно
+          </h3>
+          <li>шары</li>
+          <li>игрушки</li>
+          <li>открытки</li>
+          <li>упаковка</li>
+        </ul>
+        <a
+          href="#"
+          class="self-end font-['Oswald'] text-xs font-bold uppercase leading-[18px] tracking-widest text-[#D978AC] underline underline-offset-1"
+        >
+          смотреть каталог</a
+        >
+      </div>
+    </div>
+
+    <img
+      src="/public/images/ellipses-for-second-section/green-ellipse-second-section.png"
+      class="absolute right-0 z-10"
+    />
+
+    <img
+      src="/public/images/ellipses-for-second-section/pink-ellipse-second-section.png"
+      class="absolute left-0 z-10"
+    />
   </section>
 </template>
 
